9e60cef6a357bc9b179e5e2e0422dc32039565d6
MuhammadFaisalm <mfaisal@aucegypt.edu>
2018-10-14 16:59:10 +0200

    Adding the OctetSet first draft

---
+++ BitsManip/OctetSet.cpp
@@ -0,0 +1,31 @@
+#include "OctetSet.h"
+
+OctetSet::OctetSet(unsigned __int8 size = 1):Data(size)
+{
+}
+
+OctetSet::~OctetSet()
+{
+}
+
+void OctetSet::Insert(unsigned __int8 element)
+{
+	while (Data.getCapacity() < element) {
+		Data.changeOctetCapacity(2 * Data.getOctetNum());
+	}
+	Data.ChangeElement(element, true);
+}
+
+void OctetSet::Remove(unsigned __int8 element)
+{
+	if (Data.getCapacity() >= element)
+		Data.ChangeElement(element, false);
+}
+
+bool OctetSet::Exist(unsigned __int8 element)
+{
+	if (Data.getCapacity() < element)
+		return false;
+	else
+		return Data.getElement(element);
+}
--- BitsManip/OctetSet.h
+++ BitsManip/OctetSet.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef OCTET_SET
+#define OCTET_SET
+
+#include "BitArray.h"
+
+class OctetSet
+{
+private:
+	BitArray Data;
+
+public:
+	OctetSet(unsigned __int8);
+	~OctetSet();
+
+	void Insert(unsigned __int8 element);
+	void Remove(unsigned __int8 element);
+	bool Exist(unsigned __int8 element);
+};
+#endif
